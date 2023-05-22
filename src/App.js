dcd3c4c0112c64705054d2e0fa1f768775755b6a
Madhuban Khatri <madhuban211998@gmail.com>
2023-05-22 18:05:00 +0530

    Update App.js
    Code Updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,110 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { Grid, TextField, Button, Box } from '@mui/material';
+import shadows from '@mui/material/styles/shadows';
 
 function App() {
+  const [cityName, setCity] = useState(
+    {
+      city: "",
+      country: ""
+    }
+  );
+  const [details, setDetails] = useState(
+    {temp: "TEMPRATURE", 
+    humidity: "HUMIDITY",
+    cloud: "CLOUD",
+    wind_speed: "WIND SPEED",
+    wind_dir: "WIND DIRECTION",
+    condition: "CONDITION",
+    img: ""}
+    )
+
+  useEffect(() => {
+    console.log('City is now: ', cityName);
+  }, [cityName]);
+
+  function handleAPI(event){
+    event.preventDefault();
+    const city = event.target.city_name.value;
+    setCity(city);
+    
+    axios.get('http://api.weatherapi.com/v1/current.json?key=4f2aa6c0f2bf4bc186151423232205&q='+city+'&aqi=no')
+    .then((res)=>[setDetails({temp:res.data.current.temp_c, humidity: res.data.current.humidity, cloud: res.data.current.cloud, wind_dir: res.data.current.wind_dir, condition: res.data.current.condition.text, wind_speed: res.data.current.wind_kph, img: res.data.current.condition.icon}),setCity({city:res.data.location.name, country: res.data.location.country})])
+    .catch((err)=>console.log(err.message))
+  }
+
+
+  function handleCel(){
+
+    axios.get('http://api.weatherapi.com/v1/current.json?key=4f2aa6c0f2bf4bc186151423232205&q='+cityName+'&aqi=no')
+    .then((res)=>setDetails({temp: res.data.current.temp_c}))
+    .catch((err)=>console.log(err))
+  }
+
+  function handleFar(){
+
+    axios.get('http://api.weatherapi.com/v1/current.json?key=4f2aa6c0f2bf4bc186151423232205&q='+cityName+'&aqi=no')
+    .then((res)=>setDetails({temp: res.data.current.temp_f}))
+    .catch((err)=>console.log(err))
+  }
+
+  function testApi(){
+
+    axios.get('http://api.weatherapi.com/v1/current.json?key=4f2aa6c0f2bf4bc186151423232205&q='+'London'+'&aqi=no')
+    .then((res)=>console.log(res.data.current.condition.text))
+    .catch((err)=>console.log(err))
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Wheather App using ReactJS & Wheather API</h1>
+        <form onSubmit={handleAPI}>
+          <TextField id="outlined-basic" color="warning" name='city_name' label="City/State/Country" style={{background:"white", width:"580px"}} variant="filled" />
+        
+          <Button type='submit' style={{paddingTop:"15px",paddingBottom:"15px", marginLeft: "10px"}} color="warning" variant="contained">Check</Button>
+        </form>
+       
+        <Grid container>
+          <Grid item sm={6}>
+            <Box>
+              <br/>
+              <span className='my_span' style={{color:"#ED6C02"}}>City: </span>{cityName.city}
+            </Box>
+            <Box>
+              <br/>
+              <span className='my_span' style={{color:"#ED6C02"}}>Country: </span>{cityName.country}
+            </Box>
+            <Box>
+              <p>{details.condition}</p>
+              <img src={details.img} width={"150px"}/>  
+            </Box>
+            
+          </Grid>
+          
+          <Grid item sm={6}>
+            <br/>
+            <Box>
+              <span className='my_span'>Temperature: </span>{details.temp} C
+            </Box>
+            <br/>
+            <Box>
+              <span className='my_span'>Humidity: </span>{details.humidity}
+            </Box>
+            <br/>
+            <Box>
+              <span className='my_span'>Wind Speed: </span>{details.wind_speed} KPH
+            </Box>
+            <br/>
+            <Box>
+              <span className='my_span'>Wind Direction: </span>{details.wind_dir} 
+            </Box>
+            <br/>
+          </Grid>
+        </Grid>
+
       </header>
     </div>
   );
